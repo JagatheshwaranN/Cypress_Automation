d84d6ce4f4841880cb14abbb6800b832f1c46c66
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-01-18 11:25:02 +0530

    Navigation & Screenshot Scenarios Updated

--- cypress/e2e/3-my-scenarios/navigate.cy.js
+++ cypress/e2e/3-my-scenarios/navigate.cy.js
@@ -1,18 +1,40 @@
 describe('Browser Navigation Demo', ()=>{
 
-    it('Verify browser navigation', ()=>{
+beforeEach('Verify Launch', ()=>{
+    cy.visit('https://example.cypress.io');
+})
 
-        cy.visit('https://demo.opencart.com/');
-        cy.xpath('//span[text()="Shopping Cart"]').click();
-        cy.title().should('eq','Shopping Cart');
-        cy.go('back');
-        cy.title().should('eq','Your Store');
-        cy.go('forward');
-        cy.title().should('eq','Shopping Cart');
-        cy.go(-1);
-        cy.title().should('eq','Your Store');
-        cy.go(1);
-        cy.title().should('eq','Shopping Cart');
-        cy.reload();
+it('Visit any sub-domain of your current domain', ()=>{
+
+    cy.visit('https://example.cypress.io/commands/navigation', {
+        timeout: 50000,
+        onBeforeLoad(window) {
+            expect(typeof window === 'object').to.be.true
+        },
+        onLoad(window){
+            expect(typeof window === 'object').to.be.true
+        }
     })
+})
+})
+
+describe('Browser Navigation Demo', ()=>{
+    
+it('Verify browser navigation', ()=>{
+
+    cy.visit('https://demo.opencart.com/');
+    cy.xpath('//span[text()="Shopping Cart"]').click();
+    cy.title().should('eq','Shopping Cart');
+    cy.go('back');
+    cy.title().should('eq','Your Store');
+    cy.go('forward');
+    cy.title().should('eq','Shopping Cart');
+    cy.go(-1);
+    cy.title().should('eq','Your Store');
+    cy.go(1);
+    cy.title().should('eq','Shopping Cart');
+    cy.reload();
+    // Reload the page without cache
+    // cy.reload(true);
+})
 })
--- cypress/e2e/3-my-scenarios/screenshot.cy.js
+++ cypress/e2e/3-my-scenarios/screenshot.cy.js
@@ -6,4 +6,12 @@ describe('Capture Screenshot Demo', ()=>{
         cy.screenshot('opencart');
         cy.get('#menu').screenshot('opencart_menu');
     })
+
+    it.only('Verify capture screenshot demo', ()=>{
+
+        cy.visit('https://demo.opencart.com/');
+        cy.screenshot('opencart', {overwrite:true});
+        cy.get('#menu').screenshot('opencart_menu_padding', {padding: 20});
+    })
+
 })
