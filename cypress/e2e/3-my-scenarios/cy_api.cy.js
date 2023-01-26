bbe768fd47a97f9c5b4c26e5ad8516a81e8c6776
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2023-01-26 10:46:36 +0530

    Cypress API Scenarios Updated

--- cypress/e2e/3-my-scenarios/cy_api.cy.js
+++ cypress/e2e/3-my-scenarios/cy_api.cy.js
@@ -1,36 +1,54 @@
 context('Cypress APIs Demo', ()=>{
 
-    it('Verify CPU Arch Name of OS', ()=>{
+it('Verify CPU Arch Name of OS', ()=>{
 
-        expect(Cypress.arch).to.be.exist;
-        cy.log(Cypress.arch);
+    expect(Cypress.arch).to.be.exist;
+    cy.log(Cypress.arch);
+})
+
+// Not sure about the usecase
+it('Set & Get Cypress Configurations', ()=>{
+
+    let config = Cypress.config();
+    expect(config).to.have.property('baseUrl', null);
+    expect(Cypress.config('pageLoadTimeout')).to.eq(60000);
+    Cypress.config('pageLoadTimeout', 20000);
+    expect(Cypress.config('pageLoadTimeout')).to.eq(20000);
+    Cypress.config('pageLoadTimeout', 60000);  
+})
+
+it('Verify Env Variable', ()=>{
+
+    cy.log(Cypress.env());
+    cy.log(Cypress.env('foo'));
+    cy.log(Cypress.env('baz'));
+    Cypress.env({
+        foo: 'foo',
+        host:'localhost'
     })
+    Cypress.env('base','base-url');
+    cy.log(Cypress.env());
+    cy.log(Cypress.env('foo'));
+    cy.log(Cypress.env('baz'));
+    cy.log(Cypress.env('host'));
+    cy.log(Cypress.env('base'));
+})
 
-    // Not sure about the usecase
-    it('Set & Get Cypress Configurations', ()=>{
+it('Verify OS Name', ()=>{
 
-        let config = Cypress.config();
-        expect(config).to.have.property('baseUrl', null);
-        expect(Cypress.config('pageLoadTimeout')).to.eq(60000);
-        Cypress.config('pageLoadTimeout', 20000);
-        expect(Cypress.config('pageLoadTimeout')).to.eq(20000);
-        Cypress.config('pageLoadTimeout', 60000);  
-    })
+    expect(Cypress.platform).to.be.exist;
+    cy.log(Cypress.platform);
+})
 
-    it.only('Verify Env Variable', ()=>{
+it('Verify Cypress Version', ()=>{
 
-        cy.log(Cypress.env());
-        cy.log(Cypress.env('foo'));
-        cy.log(Cypress.env('baz'));
-        Cypress.env({
-            foo: 'foo',
-            host:'localhost'
-        })
-        Cypress.env('base','base-url');
-        cy.log(Cypress.env());
-        cy.log(Cypress.env('foo'));
-        cy.log(Cypress.env('baz'));
-        cy.log(Cypress.env('host'));
-        cy.log(Cypress.env('base'));
-    })
+    expect(Cypress.version).to.be.exist;
+    cy.log(Cypress.version);
+})
+
+it('Verify Spec File', ()=>{
+
+    cy.wrap(Cypress.spec).should('include.keys',['name', 'relative', 'absolute']);
+})
+
 })
